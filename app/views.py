f1750cde6b17aa055dad00ea8dc6b2b2cac5f899
Matthew Cross <matthewkacross@gmail.com>
2025-03-01 21:59:28 -0500

    Completed lab 3

--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+from app.forms import ContactForm
+from app import mail
+from flask_mail import Message
 
 
 ###
@@ -16,6 +19,20 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    form = ContactForm()
+    if form.validate_on_submit():
+        msg = Message(request.form['subject'],
+        sender=(request.form['name'], request.form['email']),
+        recipients=["to@example.com"])
+        msg.body = request.form['message']
+        mail.send(msg)
+
+        flash('Message sent successfully!', 'success')
+        return redirect('/')  # Prevent form resubmission
+    return render_template('contact.html', form=form)
 
 
 ###
